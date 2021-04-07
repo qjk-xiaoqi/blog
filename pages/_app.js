a6ba7cdf2e0d33a0bc5757950844f34ab3660cb3
wanmian <wanmian@bytedance.com>
2021-04-07 19:51:26 +0800

    feat: 博客系统

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,5 @@
 import App from 'next/app'
-import "../styles/pages/comm.css"
-import "antd/dist/antd.css"
+import '../styles/pages/comm.css'
+import 'antd/dist/antd.css'
 
- 
 export default App
--- pages/detail.js
+++ pages/detail.js
@@ -1,99 +1,107 @@
-import React from 'react';
+import React from 'react'
 import Head from 'next/head'
 import Link from 'next/link'
 import Header from '../components/Header'
 import Author from '../components/Author'
 import Advert from '../components/Advert'
 import Footer from '../components/Footer'
-import {Col,Row,Breadcrumb,Affix} from 'antd'
-import ReactMarkdown from "react-markdown"
-import MarkNav from "markdown-navbar"
-import "markdown-navbar/dist/navbar.css"
-import {CalendarOutlined,VideoCameraOutlined,UserOutlined} from "@ant-design/icons"
+import { Col, Row, Breadcrumb, Affix } from 'antd'
+import ReactMarkdown from 'react-markdown'
+import MarkNav from 'markdown-navbar'
+import 'markdown-navbar/dist/navbar.css'
+import { CalendarOutlined, VideoCameraOutlined, UserOutlined } from '@ant-design/icons'
 import '../styles/pages/detail.css'
 
-
 export default function Detail() {
-  const markdown='# p01:课程介绍和环境搭建\n' +
-  '[ **M** ] arkdown + E [ **ditor** ] = **Mditor**  \n' +
-  '> Mditor 是一个简洁、易于集成、方便扩展、期望舒服的编写 markdown 的编辑器，仅此而已... \n\n' +
-   '**这是加粗的文字**\n\n' +
-  '*这是倾斜的文字*`\n\n' +
-  '***这是斜体加粗的文字***\n\n' +
-  '~~这是加删除线的文字~~ \n\n'+
-  '\`console.log(111)\` \n\n'+
-  '# p02: 初始Vue3.0\n' +
-  '> aaaaaaaaa\n' +
-  '>> bbbbbbbbb\n' +
-  '>>> cccccccccc\n'+
-  '***\n\n\n' +
-  '# p03:Vue3.0基础知识讲解\n' +
-  '> aaaaaaaaa\n' +
-  '>> bbbbbbbbb\n' +
-  '>>> cccccccccc\n\n'+
-  '# p04:Vue3.0基础知识讲解\n' +
-  '> aaaaaaaaa\n' +
-  '>> bbbbbbbbb\n' +
-  '>>> cccccccccc\n\n'+
-  '# p05:Vue3.0基础知识讲解\n' +
-  '> aaaaaaaaa\n' +
-  '>> bbbbbbbbb\n' +
-  '>>> cccccccccc\n\n'+
-  '# p06:Vue3.0基础知识讲解\n' +
-  '> aaaaaaaaa\n' +
-  '>> bbbbbbbbb\n' +
-  '>>> cccccccccc\n\n'+
-  '# p07:Vue3.0基础知识讲解\n' +
-  '> aaaaaaaaa\n' +
-  '>> bbbbbbbbb\n' +
-  '>>> cccccccccc\n\n'+
-  '``` var a=11; ```'
+  const markdown =
+    '# p01:课程介绍和环境搭建\n' +
+    '[ **M** ] arkdown + E [ **ditor** ] = **Mditor**  \n' +
+    '> Mditor 是一个简洁、易于集成、方便扩展、期望舒服的编写 markdown 的编辑器，仅此而已... \n\n' +
+    '**这是加粗的文字**\n\n' +
+    '*这是倾斜的文字*`\n\n' +
+    '***这是斜体加粗的文字***\n\n' +
+    '~~这是加删除线的文字~~ \n\n' +
+    '`console.log(111)` \n\n' +
+    '# p02: 初始Vue3.0\n' +
+    '> aaaaaaaaa\n' +
+    '>> bbbbbbbbb\n' +
+    '>>> cccccccccc\n' +
+    '***\n\n\n' +
+    '# p03:Vue3.0基础知识讲解\n' +
+    '> aaaaaaaaa\n' +
+    '>> bbbbbbbbb\n' +
+    '>>> cccccccccc\n\n' +
+    '# p04:Vue3.0基础知识讲解\n' +
+    '> aaaaaaaaa\n' +
+    '>> bbbbbbbbb\n' +
+    '>>> cccccccccc\n\n' +
+    '# p05:Vue3.0基础知识讲解\n' +
+    '> aaaaaaaaa\n' +
+    '>> bbbbbbbbb\n' +
+    '>>> cccccccccc\n\n' +
+    '# p06:Vue3.0基础知识讲解\n' +
+    '> aaaaaaaaa\n' +
+    '>> bbbbbbbbb\n' +
+    '>>> cccccccccc\n\n' +
+    '# p07:Vue3.0基础知识讲解\n' +
+    '> aaaaaaaaa\n' +
+    '>> bbbbbbbbb\n' +
+    '>>> cccccccccc\n\n' +
+    '``` var a=11; ```'
   return (
     <>
       <Head>
         <title>博客详情页</title>
       </Head>
-      <Header/> 
+      <Header />
       <Row className="comm-main" type="flex" justify="center">
-        <Col className="comm-left"   xs={24} sm={24} md={16} lg={18} xl={14}>
+        <Col className="comm-left" xs={24} sm={24} md={16} lg={18} xl={14}>
           <div>
             <div className="bread-box">
               <Breadcrumb>
-                <Breadcrumb.Item><a href="/">首页</a></Breadcrumb.Item>
+                <Breadcrumb.Item>
+                  <a href="/">首页</a>
+                </Breadcrumb.Item>
                 <Breadcrumb.Item>视频列表</Breadcrumb.Item>
                 <Breadcrumb.Item>XXXX</Breadcrumb.Item>
               </Breadcrumb>
             </div>
           </div>
           <div>
-            <div className="detail-title">
-              TS 基础总结
-            </div>
+            <div className="detail-title">TS 基础总结</div>
             <div className="list-icon list-center">
-              <span> <CalendarOutlined /> 2020-10-9</span>
-              <span> <VideoCameraOutlined />视频教程</span>
-              <span><UserOutlined />5283人</span>
+              <span>
+                {' '}
+                <CalendarOutlined /> 2020-10-9
+              </span>
+              <span>
+                {' '}
+                <VideoCameraOutlined />
+                视频教程
+              </span>
+              <span>
+                <UserOutlined />
+                5283人
+              </span>
             </div>
-            
+
             <div className="detail-content">
-              <ReactMarkdown source={markdown} escapeHTML={false}/>
+              <ReactMarkdown source={markdown} escapeHTML={false} />
             </div>
           </div>
-       
         </Col>
-        <Col className="comm-right"   xs={0} sm={0} md={7} lg={5} xl={4}>
-           <Author/>
-           <Advert/>
-           <Affix offsetTop={5} >
+        <Col className="comm-right" xs={0} sm={0} md={7} lg={5} xl={4}>
+          <Author />
+          <Advert />
+          <Affix offsetTop={5}>
             <div className="detail-nav comm-box">
               <div className="nav-title">文章目录</div>
-              <MarkNav className="article-menu" source={markdown} ordered={false}/>
+              <MarkNav className="article-menu" source={markdown} ordered={false} />
             </div>
-           </Affix>
-          
+          </Affix>
         </Col>
       </Row>
-      <Footer/>
+      <Footer />
     </>
   )
 }
